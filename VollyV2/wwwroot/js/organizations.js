f70d8e48fb5607d74b80965104b5e0692e9c3a5f
Mo <mo_xue1989@yahoo.ca>
2019-03-12 18:30:27 -0600

    show map on org page

--- VollyV2/wwwroot/js/organizations.js
+++ VollyV2/wwwroot/js/organizations.js
@@ -54,7 +54,7 @@ function appendOrganizationPanel(organization) {
 
 function initMap() {
     map = new GMaps({
-        div: '#map',
+        div: '#map2',
         lat: 51.044308,
         lng: -114.0652801,
         zoom: 14
